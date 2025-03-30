4396e4ff5361a88dd6d6b9690f5ba0368aa19c26
Hồng Sơn <vansonpham10112k1@gmail.com>
2025-03-30 15:53:46 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +11,10 @@ async function bootstrap() {
     origin: '*',
   });
 
+  app.use(
+    '/uploads',
+    express.static(join(__dirname, '..', 'public', 'uploads')),
+  );
   app.use(bodyParser.json({ limit: '1000mb' }));
   app.use(bodyParser.urlencoded({ limit: '1000mb', extended: true }));
 
